c00e728a2baa4ae2e6c9fbe450ea5628dc58db2a
Łukasz <60322@dsw.edu.pl>
2026-06-26 19:48:13 +0200

    Dodane --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,14 @@
 
 if [ "$1" = "--date" ]; then
     date
+
+elif [ "$1" = "--logs" ]; then
+	for i in $(seq 1 100)
+	do
+		nazwaPliku="log$i.txt"
+
+		echo "Nazwa pliku: $nazwaPliku" > "$nazwaPliku"
+		echo "Utworzony przez skrypt: $0" >> "$nazwaPliku"
+		echo "Data utworzenia: $(date)" >> "$nazwaPliku"
+	done
 fi
