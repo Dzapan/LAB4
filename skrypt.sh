e8b06e26f010a3f6f6f7b66a56b65096a53615e7
Łukasz <60322@dsw.edu.pl>
2026-06-26 19:59:03 +0200

    dodanie --init

--- skrypt.sh
+++ skrypt.sh
@@ -21,6 +21,33 @@ elif [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "--logs, -l          Tworzy 100 plikow log"
     echo "--logs LICZBA        Tworzy podana liczbe plikow log"
 
+elif [ "$1" = "--init" ]; then
+    REPO_URL="https://github.com/Dzapan/LAB4.git"
+    REPO_NAME="NBI-Lab4-Git"
+    KATALOG_DOCELOWY="$PWD/$REPO_NAME"
+
+    if [ -d "$KATALOG_DOCELOWY" ]; then
+        echo "Katalog $KATALOG_DOCELOWY juz istnieje."
+        exit 1
+    fi
+
+    git clone "$REPO_URL" "$KATALOG_DOCELOWY"
+
+    if [ "$?" -ne 0 ]; then
+        echo "Blad podczas klonowania repozytorium."
+        exit 2
+    fi
+
+    chmod +x "$KATALOG_DOCELOWY/skrypt.sh"
+
+    echo "export PATH=\"\$PATH:$KATALOG_DOCELOWY\"" >> "$HOME/.bashrc"
+    export PATH="$PATH:$KATALOG_DOCELOWY"
+
+    echo "Repozytorium zostalo sklonowane do:"
+    echo "$KATALOG_DOCELOWY"
+    echo "Katalog zostal dodany do PATH."
+    echo "Uruchom: source ~/.bashrc"
+
 else
     echo "Nieznana opcja. Uzyj --help lub -h."
 fi
